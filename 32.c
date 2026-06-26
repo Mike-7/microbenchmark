fe1483bbd80537ce95ebb1e76eb677969f97f451
Mike-7 <ciesielskimichal7@gmail.com>
2026-06-27 01:53:24 +0200

    fix(32): code style

--- 32.c
+++ 32.c
@@ -4,7 +4,8 @@
 int main(void)
 {
     int32_t result = 0;
-    for (volatile int32_t i = 0; i < 1000000; i++) {
+    for (volatile int32_t i = 0; i < 1000000; i++)
+	{
         result += 1;
     }
     return 0;
